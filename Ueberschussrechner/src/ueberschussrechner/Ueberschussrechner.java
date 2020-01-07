2e4160b916b5581aeebe60da56940b976993b2a0
lukasdw <l.deweerdt@outlook.de>
2020-01-07 14:46:55 +0100

    test

--- Ueberschussrechner/src/ueberschussrechner/Ueberschussrechner.java
+++ Ueberschussrechner/src/ueberschussrechner/Ueberschussrechner.java
@@ -11,11 +11,8 @@ package ueberschussrechner;
  */
 public class Ueberschussrechner {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("test");
     }
     
 }
