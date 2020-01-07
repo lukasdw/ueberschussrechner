393ad1f00f497b82ab2196394dbcb70079234f56
lukasdw <l.deweerdt@outlook.de>
2020-01-07 14:53:25 +0100

    test

--- Ueberschussrechner/src/ueberschussrechner/Ueberschussrechner.java
+++ Ueberschussrechner/src/ueberschussrechner/Ueberschussrechner.java
@@ -12,6 +12,8 @@ package ueberschussrechner;
 public class Ueberschussrechner {
 
     public static void main(String[] args) {
+        
+        System.out.println("Ueberschussrechner.exe");
         System.out.println("test");
     }   
 }
